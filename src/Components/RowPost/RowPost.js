51399832de300e8c8cf5a21d80a9eb8915bbe222
Jishnu123 <jishnu@nextgbl.com>
2024-07-01 16:42:16 +0530

    feature commit

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -2,11 +2,11 @@ import React,{useEffect,useState} from 'react'
 import './RowPost.css'
 import axios from '../../axios'
 import {imageUrl} from '../../constants/constant'
-
-
+import YouTube from 'react-youtube'
+import {API_KEY} from '../../constants/constant'
 function RowPost(props) {
   const [movies, setMovies] = useState();
-
+const[urlId,setUrlId]=useState();
   useEffect(() => {
    axios.get(props.url).then(response=>{
     console.log(response.data)
@@ -15,20 +15,47 @@ function RowPost(props) {
     alert('Network error')
    })
       
-  }, []);
+  });
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie=(id)=>{
+console.log(id)
+axios.get(`/movie/${id}/videos?language=en-US&api_key=${API_KEY}`).then(response=>{
+  if(response.data.results.length!==0){
+   console.log(response.data)
+    setUrlId(response.data.results[0])
+  }
+  else{
+    console.log('Array empty')
+  }
+})
+  }
   return (
     <div 
     className='row'>
         <h2>{props.title}</h2>
         <div className="posters">
-          {movies?movies.map((obj)=>
-          
-            <img className={props.isSmall?'smallPoster':'poster'}alt='poster' src={`${imageUrl+obj.backdrop_path}`}/>
-          ):""}
+        
+        {movies?movies.map((obj)=>
+    
+
+
+        <img onClick={()=>handleMovie(obj.id)} className={props.isSmall?'smallPoster':'poster'}alt='poster' src={`${imageUrl+obj.backdrop_path}`}/>  
+      
+
+
+        ):""}
 
         </div>
-      
-    </div>
+       { urlId && <YouTube videoId={urlId.key} opts={opts}/> }  ;  
+        </div>
   )
 }
 
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,3 @@
 export const baseUrl='https://api.themoviedb.org/3';
-export const API_KEY='cd3b5ebfdc9724613b77c645fcf83743';
+export const API_KEY='551f2e1b791f2ac9e3298cc164efa23d';
 export const imageUrl='https://image.tmdb.org/t/p/original'
